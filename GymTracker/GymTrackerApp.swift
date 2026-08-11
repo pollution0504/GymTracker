e8321f385746a32d216b11a41bdcf6be6f03f31e
Noah Poulatian <noahp9922@gmail.com>
2026-08-10 17:13:38 -0700

    Initial Commit

---
+++ GymTracker/GymTrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  GymTrackerApp.swift
+//  GymTracker
+//
+//  Created by Noah Poulatian on 8/10/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GymTrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ GymTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GymTracker
+//
+//  Created by Noah Poulatian on 8/10/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
